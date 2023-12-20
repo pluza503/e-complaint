be4b615f23a10173c15c466e5a1dfc90446443fd
Firstsouharit38 <souharitmophsurat@gmail.com>
2023-12-20 10:56:02 +0700

    open menu

--- modules/repair/controllers/initmenu.php
+++ modules/repair/controllers/initmenu.php
@@ -36,13 +36,13 @@ class Controller extends \Kotchasan\KBase
         $submenus = array(
             array(
                 'text' => '{LNG_Get a repair}',
-                //'url' => 'index.php?module=repair-receive'
-                'url' => 'index.php?'
+                'url' => 'index.php?module=repair-receive'
+                // 'url' => 'index.php?' <<<< ปิดเมนูแจ้งเรื่องร้องเรียน comment บรรทัดบนออก
             ),
             array(
                 'text' => '{LNG_Repair history}',
-                //'url' => 'index.php?module=repair-history'
-                'url' => 'index.php?'
+                'url' => 'index.php?module=repair-history'
+                // 'url' => 'index.php?' <<<< ปิดเมนูติดตามเรื่องร้องเรียน comment บรรทัดบนออก
             )
         );
         // สามารถจัดการรายการซ่อมได้, ช่างซ่อม
